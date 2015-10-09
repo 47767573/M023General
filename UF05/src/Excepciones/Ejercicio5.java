40e28e2b2dffdc20e793bbe262a47a72d55ea500
Moises <47767573t@iespoblenou.org>
2015-10-09 20:15:59 +0200

    Ejercicios en clase 151010

--- UF05/src/Excepciones/Ejercicio5.java
+++ UF05/src/Excepciones/Ejercicio5.java
@@ -1,12 +1,34 @@
 package Excepciones;
 
+import java.io.*;
+
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		
+		boolean salir = false;
 		
 		
+		while (!salir){
 		
+			try{
+				System.out.print("Introduce un número: ");
+				InputStreamReader isr = new InputStreamReader(System.in);	
+				BufferedReader br = new BufferedReader (isr);
+				
+				String str = br.readLine();
+				
+				int numero = Integer.parseInt(str);
+				int validarEntero = numero*numero;
+				
+				System.out.println("numero entero!");
+				
+				salir = true;				
+			
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}		
 	}
-
 }
