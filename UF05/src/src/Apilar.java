179975cc067beb9b52e2c97e6786e5ae6d2db39d
Moises <47767573t@iespoblenou.org>
2015-10-29 20:26:45 +0100

    incluido inicio de nueva practica de Apilar

---
+++ UF05/src/src/Apilar.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+/**
+ * Created by 47767573t on 29/10/15.
+ */
+public class Apilar {
+
+
+    int numMin;
+
+    public static void main(String[] args) {
+
+        int[] pila = {6};
+
+
+
+
+    }
+
+
+
+    public int calcMin (int[] pila){
+
+
+        //Calcular elemento mínimo de un array
+        /*for (int i = 0; i<= aInteger.length ; i++){
+            if (aInteger[i] < numMin) numMin = aInteger[i];
+
+
+        }*/
+
+        return 1;
+    }
+
+
+}
